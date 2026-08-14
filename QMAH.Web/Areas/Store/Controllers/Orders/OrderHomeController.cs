cc5cb0359673275bfb0cdef801b3238105c7dad6
AnChi <145381381+AnChiCode@users.noreply.github.com>
2026-08-14 17:34:05 +0800

    訂單列表、細節與對應檢視，與用於顯示列表簡化資訊的類。

---
+++ QMAH.Web/Areas/Store/Controllers/Orders/OrderHomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using QMAH.Web.Areas.Store.ViewModels;
+using QMAH.Web.Data;
+using QMAH.Web.Models.Entities;
+
+
+
+namespace QMAH.Web.Areas.Store.Controllers.Orders;
+
+[Area("Store")]
+[Route("store/order")]
+public class OrderHomeController : Controller
+{
+    private readonly QmahDbContext _db;
+
+    public OrderHomeController(QmahDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    [HttpGet("Index")]
+    public async Task<IActionResult> Index(int page = 0, int rows = 20)
+    {
+        bool isOverShot = this._db.StoreOrders.Count() < page * rows;
+        if (isOverShot) page = 0;
+
+        var ls = from order in this._db.StoreOrders.Skip(page * rows).Take(rows)
+                 join user in this._db.Users on order.UserId equals user.Id
+                 join items in this._db.OrderDetails on order.Id equals items.OrderId into itemsGroup
+                 select new OrderSimplefyListItem()
+                 {
+                     Id = order.Id,
+                     UserName = user.UserName ?? string.Empty,
+                     Status = order.Status,
+                     ItemsCount = itemsGroup.Sum(v => v.Quantity),
+                     ItemsTotal = order.Subtotal,
+                     DiscountAmount = order.DiscountAmount,
+                     PointUsed = order.PointsUsed,
+                     Total = order.TotalAmount,
+                     CreateAt = order.CreatedAt,
+                     CancelledAt = order.CancelledAt,
+                     PaidAt = order.PaidAt
+                 };
+
+        return View(ls);
+    }
+
+    [HttpGet("{id:Guid}")]
+    public async Task<IActionResult> GetOrder(Guid id)
+    {
+        StoreOrder? order = this._db.StoreOrders.FirstOrDefault(v => v.Id == id);
+        if (order == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View(order);
+    }
+}
---
+++ QMAH.Web/Areas/Store/ViewModels/OrderSimplefyListItem.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QMAH.Web.Areas.Store.ViewModels;
+
+public class OrderSimplefyListItem
+{
+    [Display(Name = "訂單編號")]
+    public required Guid Id { get; set; }
+    [Display(Name = "下訂會員名稱")]
+    public required string UserName { get; set; }
+    [Display(Name = "狀態")]
+    public required string Status { get; set; }
+    [Display(Name = "商品數量")]
+    public required int ItemsCount { get; set; }
+    [Display(Name = "商品小計")]
+    public required decimal ItemsTotal { get; set; }
+    [Display(Name = "折扣金額")]
+    public required decimal DiscountAmount { get; set; }
+    [Display(Name = "點數消耗")]
+    public required decimal PointUsed { get; set; }
+    [Display(Name = "總計")]
+    public required decimal Total { get; set; }
+    [Display(Name = "建立時間")]
+    public required DateTime CreateAt { get; set; }
+    [Display(Name = "付款時間")]
+    public DateTime? PaidAt { get; set; }
+    [Display(Name = "取消時間")]
+    public DateTime? CancelledAt { get; set; }
+}
