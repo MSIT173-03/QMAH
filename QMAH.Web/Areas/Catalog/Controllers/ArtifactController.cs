e20f4a64ae20c9def551d9224f2ddaf0628e5dd0
洪文彥 <lenbossop@gmail.com>
2026-08-15 00:29:47 +0800

    1.修改顯示的文字敘述&內容 2.List查詢方法新增類別查詢 3.新增編輯文物的功能(樣板參考置網上)

--- QMAH.Web/Areas/Catalog/Controllers/ArtifactController.cs
+++ QMAH.Web/Areas/Catalog/Controllers/ArtifactController.cs
@@ -1,4 +1,5 @@
 ﻿using DocumentFormat.OpenXml.InkML;
+using DocumentFormat.OpenXml.Office2010.Excel;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -14,79 +15,168 @@ namespace QMAH.Web.Areas.Catalog.Controllers;
 [Area("Catalog")]
 public class ArtifactController : Controller
 {
-        private readonly QmahDbContext _db;
+    private readonly QmahDbContext _db;
 
-        public ArtifactController(QmahDbContext db)
+    public ArtifactController(QmahDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<IActionResult> List(C_KeywordViewModel vm, Guid? eraBucketId, Guid? categoryId, CancellationToken cancellationToken)
+    {
+        IEnumerable<Artifact> datas_art = null;
+
+        var artifacts = await _db.Artifacts
+            .AsNoTracking()
+            .Include(artifact => artifact.Category)
+            .Include(artifact => artifact.EraBucket)
+            .OrderBy(artifact => artifact.Name)
+            .ToListAsync(cancellationToken);
+
+
+        var eraBuckets = await _db.EraBuckets
+           .OrderBy(e => e.Name)
+           .ToListAsync();
+
+
+        var category = await _db.ArtifactCategories
+           .OrderBy(e => e.Name)
+           .ToListAsync();
+
+        ViewBag.ArtifactCategoriesList = new SelectList(category, "Id", "Name", categoryId);
+        ViewBag.EraBucketList = new SelectList(eraBuckets, "Id", "Name", eraBucketId);
+
+        if (string.IsNullOrEmpty(vm.txtKeyword) && eraBucketId == null && categoryId == null)
         {
-            _db = db;
+            datas_art = from t in artifacts
+                        select t;
         }
-
-        public async Task<IActionResult> List(C_KeywordViewModel vm, Guid? eraBucketId , CancellationToken cancellationToken)
+        else
         {
-            IEnumerable<Artifact> datas_art = null;
-
-            var artifacts = await _db.Artifacts
-                .AsNoTracking()
-                .Include(artifact => artifact.Category)
-                .Include(artifact => artifact.EraBucket)
-                .Where(artifact => artifact.IsActive == true)
-                .OrderBy(artifact => artifact.Name)
-                .ToListAsync(cancellationToken);
-                
-
-             var eraBuckets = await _db.EraBuckets
-                .OrderBy(e => e.Name)
-                .ToListAsync();
-
-            ViewBag.EraBucketList = new SelectList(eraBuckets, "Id", "Name", eraBucketId);
-
-            if (string.IsNullOrEmpty(vm.txtKeyword) && eraBucketId==null)
+            if (!string.IsNullOrEmpty(vm.txtKeyword) && eraBucketId != null && categoryId != null)
             {
-                datas_art = from t in artifacts
-                select t;
-            }
-           else
-           {
-                if (!string.IsNullOrEmpty(vm.txtKeyword) && eraBucketId!=null)
-                {
-                datas_art = artifacts.Where(t => t.Name.Contains(vm.txtKeyword)
-                    || t.EraTextOriginal.Contains(vm.txtKeyword) && t.EraBucketId == eraBucketId);
-                }
-                else
-                {
-                    if (eraBucketId!= null)
-                    {
-                        datas_art = from t in artifacts
-                                where t.EraBucketId ==eraBucketId
-                                select t;
-                    ViewBag.SelectedEraBucketId = eraBucketId;
-                     }
-                    else
-                    {
-                        datas_art = artifacts.Where(t => t.Name.Contains(vm.txtKeyword)
-                        ||t.EraTextOriginal.Contains(vm.txtKeyword));
-                    }
-                }
-                
-
-           }
-           ViewBag.SelectedEraBucketId = eraBucketId;
-           return View(datas_art);
-         }
-
-        public ActionResult Delete(Guid? id)
-        {
-            var artifacts = _db.Artifacts;
-            Artifact a = _db.Artifacts.FirstOrDefault(t => t.Id == id);
-            if (a != null)
-            {
-                a.IsActive = false;
-                _db.SaveChanges();
+                datas_art = artifacts.Where(t => t.EraBucketId == eraBucketId
+                && t.CategoryId == categoryId
+                && t.Name.Contains(vm.txtKeyword)
+                || t.EraTextOriginal.Contains(vm.txtKeyword)
+                || t.ArtifactRef.Contains(vm.txtKeyword));
             }
             else
             {
-            return Content("Id 不存在");
+                if (!string.IsNullOrEmpty(vm.txtKeyword) && eraBucketId != null)
+                {
+                    datas_art = artifacts.Where(t => t.EraBucketId == eraBucketId
+                    && t.Name.Contains(vm.txtKeyword)
+                    || t.EraTextOriginal.Contains(vm.txtKeyword)
+                    || t.ArtifactRef.Contains(vm.txtKeyword));
+                }
+
+                else if (!string.IsNullOrEmpty(vm.txtKeyword) && categoryId != null)
+                {
+                    datas_art = artifacts.Where(t => t.CategoryId == categoryId
+                    && t.Name.Contains(vm.txtKeyword)
+                    || t.EraTextOriginal.Contains(vm.txtKeyword)
+                    || t.ArtifactRef.Contains(vm.txtKeyword));
+                }
+                else if (categoryId != null && eraBucketId != null)
+                {
+                    datas_art = artifacts.Where(t => t.CategoryId == categoryId
+                    && t.EraBucketId == eraBucketId);
+                }
+                else
+                {
+                    if (eraBucketId != null)
+                    {
+                        datas_art = artifacts.Where(t => t.EraBucketId == eraBucketId);
+                    }
+                    if (vm.txtKeyword != null)
+                    {
+                        datas_art = artifacts.Where(t => t.Name.Contains(vm.txtKeyword)
+                        || t.EraTextOriginal.Contains(vm.txtKeyword)
+                        || t.ArtifactRef.Contains(vm.txtKeyword));
+                    }
+                    if (categoryId != null)
+                    {
+                        datas_art = artifacts.Where(t => t.CategoryId == categoryId);
+                    }
+                }
             }
-            return RedirectToAction("List");
         }
+        ViewBag.SelectedCategory = categoryId;
+        ViewBag.SelectedEraBucketId = eraBucketId;
+        return View(datas_art);
+    }
+
+    public ActionResult Delete(Guid? id)
+    {
+        var artifacts = _db.Artifacts;
+        Artifact a = _db.Artifacts.FirstOrDefault(t => t.Id == id);
+        if (a != null)
+        {
+            a.IsActive = false;
+            _db.SaveChanges();
+        }
+        else
+        {
+            return Content("Id 不存在");
+        }
+        return RedirectToAction("List");
+    }
+
+
+    public ActionResult Edit(Guid? id, Guid eraBucketId, Guid categoryId)
+    {
+        if (id == null)
+        {
+            return Content("Id 不存在");
+        }
+        else
+        {
+            var eraBuckets = _db.EraBuckets
+                .OrderBy(e => e.Name)
+                .ToList();
+
+
+            var category = _db.ArtifactCategories
+                    .OrderBy(e => e.Name)
+                    .ToList();
+
+            var artifacts = _db.Artifacts;
+            Artifact a = _db.Artifacts.FirstOrDefault(t => t.Id == id);
+            ViewBag.ArtifactCategoriesList = new SelectList(category, "Id", "Name", a.CategoryId);
+            ViewBag.EraBucketList = new SelectList(eraBuckets, "Id", "Name", a.EraBucketId);
+            return View(a);
+        }
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Artifact af, Guid eraBucketId, Guid categoryId)
+    {
+        var artifacts = _db.Artifacts;
+        Artifact a = _db.Artifacts.FirstOrDefault(t => t.Id == af.Id);
+        if (af.Id == null)
+        {
+            return Content("Id 不存在");
+        }
+        else
+        {
+            if (af.PrimaryImagePath != null)
+            {
+                a.ThumbnailPath = af.ThumbnailPath;
+            }
+            a.ArtifactRef = af.ArtifactRef;
+            a.Name = af.Name;
+            a.CategoryId = af.CategoryId;
+            a.EraBucketId = af.EraBucketId;
+            a.EraTextOriginal = af.EraTextOriginal;
+            a.Description = af.Description;
+            a.SourceUrl = af.SourceUrl;
+            a.LicenseCode = af.LicenseCode;
+            a.AttributionText = af.AttributionText;
+            a.SizeText = af.SizeText;
+            a.IsActive = af.IsActive;
+            _db.SaveChanges();
+        }
+        return RedirectToAction("List");
+    }
 }
