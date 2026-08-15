b2d9f85aa9299823ed9ac1ec0f8e85855d5d976f
洪文彥 <lenbossop@gmail.com>
2026-08-15 16:14:59 +0800

    內容暫存同步版本

---
+++ QMAH.Web/Areas/Catalog/Controllers/KeyBackpack.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QMAH.Web.Areas.Catalog.Controllers;
+
+public class KeyBackpack : Controller
+{
+
+    public IActionResult List()
+    {
+        return View();
+    }
+
+
+    public IActionResult Unlocked()
+    {
+        return View();
+    }
+}
---
+++ QMAH.Web/Areas/Catalog/Controllers/KeyController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using QMAH.Web.Areas.Catalog.ViewModel;
+using QMAH.Web.Data;
+using QMAH.Web.Models.Entities;
+
+namespace QMAH.Web.Areas.Catalog.Controllers;
+
+public class KeyController : Controller
+{
+
+    private readonly QmahDbContext _db;
+
+    public KeyController(QmahDbContext db)
+    {
+        _db = db;
+    }
+
+
+    public async Task<IActionResult> List(C_KeywordViewModel vm, CancellationToken cancellationToken)
+    {
+        IEnumerable<Artifact> datas_keyD = null;
+
+        var artifacts = await _db.KeyDefinitions
+            .AsNoTracking()
+            .Include(keydefinitions => keydefinitions.Category)
+            .Include(keydefinitions => keydefinitions.EraBucket)
+            .OrderBy(keydefinitions => keydefinitions.Name)
+            .ToListAsync(cancellationToken);
+        return View(datas_keyD);
+    }
+    public IActionResult Create()
+    {
+        return View();
+    }
+    public IActionResult delete()
+    {
+        return View();
+    }
+    public IActionResult Edit()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(int? id)
+    {
+        return View();
+    }
+
+
+
+}
