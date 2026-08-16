89a20e3d6ebd73a97022f7f48284acafe60d8d24
AnChi <145381381+AnChiCode@users.noreply.github.com>
2026-08-16 16:40:40 +0800

    產品列表、細節檢視。

---
+++ QMAH.Web/Areas/Store/Controllers/Products/ProductHomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using QMAH.Web.Areas.Store.ViewModels;
+using QMAH.Web.Data;
+
+namespace QMAH.Web.Areas.Store.Controllers.Products;
+
+[Area("Store")]
+[Route("store/product")]
+public class ProductHomeController : Controller
+{
+
+    private readonly QmahDbContext db;
+
+    public ProductHomeController(QmahDbContext db)
+    {
+        this.db = db;
+    }
+
+    [HttpGet]
+    [HttpGet("Index")]
+    public IActionResult Index(int page = 0, int rows = 20)
+    {
+        bool isOverShot = this.db.Products.Count() < page * rows;
+        if (isOverShot) return View(new List<ProductSimplefyListItem>());
+
+        var data = this.db.Products
+            .Skip(page * rows).Take(rows)
+            .Select(p => new ProductSimplefyListItem(p))
+            .ToList();
+
+        return View(data);
+    }
+
+    [HttpGet("{id:Guid}")]
+    public IActionResult GetProduct(Guid id)
+    {
+        var product = db.Products.Where(p => p.Id == id).FirstOrDefault();
+
+        return View(product);
+    }
+}
---
+++ QMAH.Web/Areas/Store/ViewModels/ProductSimplefyListItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QMAH.Web.Areas.Store.ViewModels;
+
+public class ProductSimplefyListItem(QMAH.Web.Models.Entities.Product product)
+{
+    [Display(Name = "編號")]
+    public Guid Id { get; } = product.Id;
+    [Display(Name = "名稱")]
+    public string Name { get; } = product.Name;
+    [Display(Name = "分類")]
+    public string Category { get; } = product.CategoryCode;
+    [Display(Name = "價格")]
+    public decimal Price { get; } = product.Price;
+    [Display(Name = "庫存")]
+    public int Stock { get; } = product.Stock;
+    [Display(Name = "封面圖片網址")]
+    public string ImageUrl { get; } = product.PrimaryImagePath ?? string.Empty;
+    [Display(Name = "上架")]
+    public bool IsActive { get; } = product.IsActive;
+}
