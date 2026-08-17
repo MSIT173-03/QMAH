0ceeda9f2c8693db388e535237bf2dbf5192a390
洪文彥 <lenbossop@gmail.com>
2026-08-17 15:51:25 +0800

    此版本存在異常，無法建立UserKeyBalance的View

--- QMAH.Web/Areas/Catalog/Controllers/KeyBackpackController.cs
+++ QMAH.Web/Areas/Catalog/Controllers/KeyBackpackController.cs
@@ -1,18 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using QMAH.Web.Data;
+using QMAH.Web.Infrastructure.AdminNavigation;
+using QMAH.Web.Models.Entities;
 
 namespace QMAH.Web.Areas.Catalog.Controllers;
 
-public class KeyBackpackController : Controller
+
+[Area("Catalog")]
+[AdminNavigation("玩家鑰匙背包一覽", order: 10)]
+public class KeyBackPackController : Controller
 {
 
-    public IActionResult List()
+    private readonly QmahDbContext _db;
+    public KeyBackPackController(QmahDbContext db)
     {
-        return View();
+        _db = db;
     }
 
 
-    public IActionResult Unlocked()
+    public async Task<ActionResult> Index(CancellationToken cancellationToken)
     {
-        return View();
+        IEnumerable<UserKeyBalance> datas_ukb = null;
+
+        var ukb = await _db.UserKeyBalances
+            .AsNoTracking()
+            .Include(UserKeyBalances => UserKeyBalances.KeyDefinition)
+            .OrderBy(UserKeyBalances => UserKeyBalances.UserId)
+            .ToListAsync(cancellationToken);
+
+        datas_ukb = from t in ukb
+                    select t;
+
+        return View(datas_ukb);
     }
 }
